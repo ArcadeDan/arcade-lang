cd482f41a9cb174653b650bba6e5448eb5fe81a7
Dan Sparks <lordaniel24@gmail.com>
2022-12-13 17:37:14 -0600

    more tokens

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::io::{stdin, BufRead};
-use std::str::FromStr;
-use std::{fs, io::BufReader};
 use anyhow::Ok;
 use anyhow::Result;
 use logos::Logos;
-
+use std::io::{stdin, BufRead};
+use std::str::FromStr;
+use std::{fs, io::BufReader};
 
 #[derive(Logos, Debug, PartialEq)]
 enum Token {
@@ -19,41 +18,46 @@ enum Token {
     #[regex(r"[0-9]+")]
     TInteger,
     #[regex(r"(([0-9]+)[.])\d+")]
-    TFloat,
-    #[token(r"%")]
+    TFloat, // floating points must have a character after the '.'
+    #[token("%")]
     TModulus,
-    #[token(r"(")]
-    TOpar,
-    #[token(r")")]
-    TCpar,
+    #[token("(")]
+    TLparen,
+    #[token(")")]
+    TRparen,
+    #[token("[")]
+    TLbrack,
+    #[token("]")]
+    TRbrack,
+    #[token("{")]
+    TLbrace,
+    #[token("}")]
+    TRbrace,
+    #[regex(r#""[^"]*""#)]
+    TString,
     #[error]
     #[regex(r"[\t\n\f]+", logos::skip)]
     ERROR,
 }
 
-
-
-
 fn main() {
-    
     for line in stdin().lock().lines() {
         let expression = line.unwrap();
         let mut lexer = Token::lexer(&expression);
         let tokens: Vec<_> = lexer.spanned().filter(|x| x.0 != Token::ERROR).collect();
         for token in tokens.iter() {
-            print!("{:?} ", token.0);
+            print!("{:?} : ", token.0);
         }
         println!("\n");
     }
 }
 #[cfg(test)]
 mod tests {
-    use logos::Logos;
     use crate::Token;
+    use logos::Logos;
 
     #[test]
-    fn tokens_test() {
+    fn negative_b() {
         todo!()
     }
-
 }
